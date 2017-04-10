f221a88e997fe5bac012bca69e62e31a20556669
wx5201 <18288795852@163.com>
2017-04-10 18:03:12 +0800

    第二次更新

--- app/src/main/java/cn/edu/wx/swfu/test3/helloActivity.java
+++ app/src/main/java/cn/edu/wx/swfu/test3/helloActivity.java
@@ -3,6 +3,18 @@ package cn.edu.wx.swfu.test3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 线性布局LIneLayout
+ * 其中控件可以按照水平或垂直的方式一次排列
+ * 使用android：orientatiion="vertical"属性可以指定为垂直
+ * 使用android：orientation=“horizontal”属性可以指定为水平，默认为水平
+ * orientation用来指定当前线性布局排布方向
+ * android：layout_weight属性
+ *
+ * margin 表示控件距离其他控件或屏幕边缘的间距....外边距
+ * padding：表示控件内部内容距离控件边缘的距离....内边距
+ */
+
 public class helloActivity extends AppCompatActivity {
 
     @Override
